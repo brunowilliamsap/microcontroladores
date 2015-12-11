ea0fa4ac07b8273085c2485e1af591c3cc20c136
vaguelima <o.lucas.lima@gmail.com>
2015-12-11 11:42:03 -0200

    Feedback e coreeção do lcd

--- principal_alternativo/principal_alternativo.ino
+++ principal_alternativo/principal_alternativo.ino
@@ -1,3 +1,5 @@
+
+
 #include<math.h>;
 //lcd
 #include <Wire.h>
@@ -29,6 +31,12 @@ int itensConsumidos=0;
 float total=0;
 //intensidadeLuz
 int intensidadeLuz=255;
+//feedBack
+int comandos=0; //comandos executados
+long int tempoInicial;
+int minutos,segundos;
+//porta inicia fechada
+int porta=0;
 void setup(){
     //LCD
     lcd.begin(16, 2);
@@ -41,6 +49,9 @@ void setup(){
     pinMode(pinLedJanela, OUTPUT);
 }
 void loop() {
+    //comunicados do hotel
+    String fromHotel="Bem vindo ao nosso Hotel!";
+    Serial.print(fromHotel);
     //lcd
     String linha1 ="Hora:";
     int hora=2;
@@ -61,15 +72,15 @@ void loop() {
     //Serial.println(leituraTemp);
     lcd.clear();
     lcd.print(linha1);//hora e conta na linha 1
-	lcd.setCursor(1,0);//status na linha 2
+    lcd.setCursor(1,0);//status na linha 2
     if (Serial.available() > 0) {
         estado = Serial.read();
+        comandos++;
         switch (estado) {
             case 'a':                                 //abrir porta
             digitalWrite(pinLedPorta, HIGH);
             digitalWrite(pinLedJanela, LOW);
             //lcd porta
-            
             lcd.print("porta           ");
             if(porta==0){
                 motor.step(48);
@@ -81,7 +92,6 @@ void loop() {
                 digitalWrite(pinLedPorta, HIGH);
                 digitalWrite(pinLedJanela, LOW);
                 //lcd porta
-                
                 lcd.print("porta              ");
                 motor.step(-48);
                 digitalWrite(pinLedPorta, LOW);
@@ -92,7 +102,6 @@ void loop() {
             digitalWrite(pinLedPorta, LOW);
             digitalWrite(pinLedJanela, HIGH);
             //lcd janela
-            
             lcd.print("Janela Abrindo");
             motor.step(48);
             digitalWrite(pinLedJanela, LOW);
@@ -101,47 +110,63 @@ void loop() {
             digitalWrite(pinLedPorta, LOW);
             digitalWrite(pinLedJanela, HIGH);
             //lcd janela
-            
             lcd.print("Janela Fechando");
             motor.step(-48);
             digitalWrite(pinLedJanela, LOW);
             break;
             case 'e':                                //aumenta a temperatura
-           
             temperatura++;
             //lcd temperatura
-            
             lcd.print(temperatura);
-			lcd.print("          ");
+            lcd.print("          ");
             delay(500);
             break;
             case 'f':                                //diminui a temperatura
-			temperatura--;
+            temperatura--;
             //lcd temperatura
-            
             lcd.print(temperatura);
-			lcd.print("          ");
+            lcd.print("          ");
             delay(500);
             break;
             case 'g':                                //liga a luz
             //lcd luz
-            
             lcd.print("Aumentar Luz");
-			 if(intensidade<245){
+            if(intensidade<245){
                 intensidade+=10;
             }
             analogWrite(pinLedLuz,intensidade);
             break;
             case 'h':
             //lcd luz
-            
             lcd.print("diminuir luz");
             if(intensidade>10){
                 intensidade-=10;
             }
             analogWrite(pinLedLuz,intensidade);
             break;
-            default':                                //diminui a intensidade da luz
+            case 'i': //comandos
+            lcd.print(comandos);
+            lcd.print(" comandos");
+            Serial.print(comandos);
+            Serial.print(" comandos");
+            case 'j': //tempo decorrido
+            //lcd luz
+            segundos=millis()*1000;
+            minutos=segundos/60;
+            if(minutos==0){
+                lcd.print(segundos);
+                lcd.print(" segundos");
+                Serial.print(segundos);
+                Serial.print(" segundos");
+            }
+            else{
+                lcd.print(minutos);
+                lcd.print(" minutos");
+                Serial.print(minutos);
+                Serial.print(" minutos");
+            }
+            break;
+            default:                                //diminui a intensidade da luz
             //lcd intensidade da luz
             lcd.print("ocioso");
             delay(500);
